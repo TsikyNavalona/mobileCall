ced399fe9863a5470b95f5cc41c0bc66fd8285a4
TsikyNavalona <tsikywatib@gmail.com>
2021-04-02 23:00:13 +0300

    'deploying'

--- src/app/home-admin/home-admin.component.ts
+++ src/app/home-admin/home-admin.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-home-admin',
@@ -15,34 +14,34 @@ export class HomeAdminComponent implements OnInit {
     //this.chartTest();
 
   }
-  chartTest() {
-    var dynamicColors = function () {
-      var r = Math.floor(Math.random());
-      var g = Math.floor(Math.random());
-      var b = Math.floor(Math.random());
-      return "rgb(" + r + "," + g + "," + b + ")";
-    };
-    var myChart = new Chart("mychart", {
-      type: 'pie',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [dynamicColors],
-          borderColor: [dynamicColors],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
+  // chartTest() {
+  //   var dynamicColors = function () {
+  //     var r = Math.floor(Math.random());
+  //     var g = Math.floor(Math.random());
+  //     var b = Math.floor(Math.random());
+  //     return "rgb(" + r + "," + g + "," + b + ")";
+  //   };
+  //   var myChart = new Chart("mychart", {
+  //     type: 'pie',
+  //     data: {
+  //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //       datasets: [{
+  //         label: '# of Votes',
+  //         data: [12, 19, 3, 5, 2, 3],
+  //         backgroundColor: [dynamicColors],
+  //         borderColor: [dynamicColors],
+  //         borderWidth: 1
+  //       }]
+  //     },
+  //     options: {
+  //       scales: {
+  //         yAxes: [{
+  //           ticks: {
+  //             beginAtZero: true
+  //           }
+  //         }]
+  //       }
+  //     }
+  //   });
+  // }
 }
